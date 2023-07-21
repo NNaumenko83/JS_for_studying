7030090615ae6bd10bb765007fe2d79ad0f7492f
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-07-21 22:40:17 +0300

    done test task

--- index.js
+++ index.js
@@ -1906,3 +1906,25 @@ The second value in the first pair in the array is 0, since the bus is empty in 
 // };
 
 // console.log(factorail(6));
+
+/*Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+*/
+
+// function findNeedle(haystack = []) {
+// 	return `found the needle at position ${haystack.findIndex(item => item === "needle")}`;
+// }
+
+// const arr = ["3", "123124234", undefined, "needle", "world", "hay", 2, "3", true, false];
+
+// console.log(findNeedle(arr));
+
+
